56e7cd83a65f8ba29ee7e431ea3996d20502874e
Cat Chapman <12089454+CatChapman@users.noreply.github.com>
2024-10-21 13:43:56 -0700

    Update ytplist.sh
    just making a comment make sense

--- ytplist.sh
+++ ytplist.sh
@@ -38,7 +38,7 @@ getpldir () {
   #extracts a relevant line from tempID.txt
 
   pldir="${pldir/\[download\]\ Downloading\ playlist\:\ /}"
-  #this removes all the unnecessary stuff to extract the playlist title so fancy_ytplist.sh knows where to dump the update.sh script
+  #this removes all the unnecessary stuff to extract the playlist title so ytplist.sh knows where to dump the update.sh script
   echo $pldir
 }
 
